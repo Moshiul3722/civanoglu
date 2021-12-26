e393ea8771a781ee4bed0a4ad25379b1df72dd29
Moshiul <gazimoshiul@gmail.com>
2021-12-26 18:25:19 +0600

    add about and contact page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function single($slug)
+    {
+       $page = Page::where('slug', $slug)->first();
+
+       if(!empty($page)){
+           return view('page',[
+               'page'=>$page
+           ]);
+       }else{
+           return abort('404');
+       }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\PropertyController;
 
 /*
@@ -22,6 +23,7 @@ Route::get('/', function () {
 Route::get('/', [HomeController::class,'home'])->name('home');
 Route::get('/properties', [PropertyController::class,'index'])->name('properties');
 Route::get('/property/{id}', [PropertyController::class,'singleProperty'])->name('singleProperty');
+Route::get('/page/{slug}', [PageController::class,'single'])->name('page');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
