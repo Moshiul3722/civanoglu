9be9b826d2025dba3694d24d458fd4ade546440e
Moshiul <gazimoshiul@gmail.com>
2021-12-27 21:02:25 +0600

    add about and contact page

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function propertyInquiry(Request $request, $property_id)
+    {
+        $request->validate([
+            'name'=>'required|max:255',
+            'phone'=>'required|max:255',
+            'email'=>'required|email|max:255',
+            'message'=>'required|max:255'
+        ]);
+
+        $contact = new Contact();
+        $contact->name = $request->name;
+        $contact->phone = $request->phone;
+        $contact->email = $request->email;
+        $contact->message = $request->message.'\n This message has been sent via'.route('singleProperty',$property_id).'website';
+        $contact->save();
+
+        return redirect(route('singleProperty',$property_id));
+    }
+}
